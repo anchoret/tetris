9e55fb50e9765ac8785559240f794a5911330d11
Korolev Aleksey <koroleffas@gmail.com>
2018-07-21 00:45:23 +0400

    Add next/current figure observables

--- src/game.ts
+++ src/game.ts
@@ -1,28 +1,35 @@
 import {Observable, BehaviorSubject, Subject} from 'rxjs';
-import {map, first, takeWhile, switchMap, share, takeUntil} from 'rxjs/operators';
+import {map, first, takeWhile, switchMap, share, takeUntil, scan, filter} from 'rxjs/operators';
 import {of} from 'rxjs/observable/of';
 import {interval} from 'rxjs/observable/interval';
 import {fromEvent} from 'rxjs/observable/fromEvent';
 import {animationFrame} from 'rxjs/scheduler/animationFrame';
 import {Figure} from './types';
 import {MAX_FPS, START_SPEED} from './constants';
-import {generateFigure} from './utils';
+import {generateFigure, calculateFigure} from './utils';
 
 let gameOver$ = new Subject();
-let gameSpeed$ = new BehaviorSubject<number>(START_SPEED);
-let ticks$ = gameSpeed$.pipe(
-    switchMap(speed => interval(Math.floor(1000 / speed))),
-    takeUntil(gameOver$),
-);
-
 let click$ = fromEvent(document, 'click');
 
 
 function createGame(fps$: Observable<number>): Observable<number> {
     console.log('START CREATE GAME');
+    let gameSpeed$ = new BehaviorSubject<number>(START_SPEED);
+    let ticks$ = gameSpeed$.pipe(
+        switchMap(speed => interval(Math.floor(1000 / speed))),
+        takeUntil(gameOver$),
+    );
     ticks$.subscribe(x => console.log('tick #' + x));
-    let nextFigure = new BehaviorSubject<Figure>(generateFigure());
-    nextFigure.subscribe(fig => console.dir(fig));
+
+    let nextFigure$ = new BehaviorSubject<Figure>(generateFigure());
+    nextFigure$.subscribe(fig => { console.log('Next>'); console.dir(fig); });
+
+    let currentFigure$ = nextFigure$.pipe(
+        scan(calculateFigure, []),
+        filter(acc => acc.length > 1),
+        map(acc => acc[0]),
+    );
+    currentFigure$.subscribe(fig => { console.log('Current>'); console.dir(fig); });
 
     return fps$.pipe(
         map(counter => counter * 2)
--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,15 @@ export function generateFigure(): Figure {
     return new Figure(startPosition, figureBody, generateColor());
 }
 
+export function calculateFigure (acc: Figure[], next: Figure): Figure[] {
+    acc.push(next);
+    if (acc.length > 2) {
+        acc.shift();
+    }
+
+    return acc;
+}
+
 function generateFigureStartPosition(figureBody: FigureBody): Point2D {
     let figureWidth = figureBody[0].length;
     let startX = Math.ceil(PLAYING_FILED_WIDTH / 2 - figureWidth / 2);
