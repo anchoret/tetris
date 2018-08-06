6571a83dc99cf4012edfad0af147a30dc53b024c
Korolev Aleksey <koroleffas@gmail.com>
2018-08-06 14:24:15 +0400

    Add rendering of the next figure

--- src/graphics.ts
+++ src/graphics.ts
@@ -1,10 +1,11 @@
 import {PLAYING_FILED_ROWS, PLAYING_FILED_COLUMNS} from './constants';
 import {Color, Figure, PlayingField, Point2D, Set} from './types';
-import {figureBodyIterator} from './utils';
+import {figureBodyIterator, getFigureHeight, getFigureWidth} from './utils';
 
 const GAP_SIZE = 2;
 const CELL_SIZE = 20;
 const ADDITIONAL_COLUMN_WIDTH = 200;
+const ADDITIONAL_COLUMN_PADDING = 20;
 const PLAYING_FILED_WIDTH = PLAYING_FILED_COLUMNS * (CELL_SIZE + GAP_SIZE) - GAP_SIZE;
 const PLAYING_FILED_HEIGHT = PLAYING_FILED_ROWS * (CELL_SIZE + GAP_SIZE) - GAP_SIZE;
 const CANVAS_WIDTH = PLAYING_FILED_WIDTH + ADDITIONAL_COLUMN_WIDTH;
@@ -23,9 +24,8 @@ export function renderPlayingField(renderingContext: CanvasRenderingContext2D, p
     renderBackground(renderingContext);
     renderScore(renderingContext, playingField.score);
     renderCurrentFigure(renderingContext, playingField.currentFigure);
+    renderNextFigure(renderingContext, playingField.nextFigure);
     renderSet(renderingContext, playingField.set);
-    // renderApples(renderingContext, scene.apples);
-    // renderSnake(renderingContext, scene.snake);
 }
 
 export function renderGameOver(renderingContext: CanvasRenderingContext2D) {
@@ -44,10 +44,6 @@ function renderScore(renderingContext: CanvasRenderingContext2D, score: number) 
 
     drawText(renderingContext, score.toString(), textX, textY, 'rgba(0, 0, 0, 0.1)', 150);
 }
-
-/*function renderApples(renderingContext: CanvasRenderingContext2D, apples: any[]) {
-    apples.forEach(apple => paintCell(renderingContext, apple, 'red'));
-}*/
 
 function renderCurrentFigure(renderingContext: CanvasRenderingContext2D, figure: Figure) {
     let initX = figure.position.x;
@@ -75,12 +71,20 @@ function renderSet(renderingContext: CanvasRenderingContext2D, set: Set) {
 }
 
 function renderNextFigure(renderingContext: CanvasRenderingContext2D, figure: Figure) {
+    let startPosition = calculateNextFigureStartPosition(figure);
 
+    figureBodyIterator(figure, (cellNumber, lineNumber, color) => {
+        let point = {
+            x: cellNumber,
+            y: lineNumber,
+        };
+        paintCell(renderingContext, point, color, startPosition.x, startPosition.y);
+    });
 }
 
-function paintCell(renderingContext: CanvasRenderingContext2D, point: Point2D, color: string) {
-    const x = point.x * CELL_SIZE + (point.x * GAP_SIZE);
-    const y = point.y * CELL_SIZE + (point.y * GAP_SIZE);
+function paintCell(renderingContext: CanvasRenderingContext2D, point: Point2D, color: string, startX: number = 0, startY: number = 0) {
+    const x = startX + point.x * CELL_SIZE + (point.x * GAP_SIZE);
+    const y = startY + point.y * CELL_SIZE + (point.y * GAP_SIZE);
 
     renderingContext.fillStyle = color;
     renderingContext.fillRect(x, y, CELL_SIZE, CELL_SIZE);
@@ -100,7 +104,39 @@ function renderMainArea(renderingContext: CanvasRenderingContext2D) {
 
 function renderNextFigureArea(renderingContext: CanvasRenderingContext2D) {
     renderingContext.fillStyle = '#b6aeee';
-    renderingContext.fillRect(PLAYING_FILED_WIDTH + 20, 0 + 20, ADDITIONAL_COLUMN_WIDTH - 40, PLAYING_FILED_HEIGHT / 2 - 40);
+    let position = getNextFigureAreaPosition();
+    let size = getNextFigureAreaSize();
+    renderingContext.fillRect(
+        position.x,
+        position.y,
+        size.width,
+        size.height,
+    );
+}
+
+function getNextFigureAreaPosition(): Point2D {
+    return {
+        x: PLAYING_FILED_WIDTH + ADDITIONAL_COLUMN_PADDING,
+        y: PLAYING_FILED_HEIGHT / 2 + ADDITIONAL_COLUMN_PADDING,
+    };
+}
+
+function getNextFigureAreaSize() {
+    return {
+        height: PLAYING_FILED_HEIGHT / 2 - 2 * ADDITIONAL_COLUMN_PADDING,
+        width: ADDITIONAL_COLUMN_WIDTH - 2 * ADDITIONAL_COLUMN_PADDING,
+    };
+}
+
+function calculateNextFigureStartPosition(figure: Figure): {x: number, y: number} {
+    let parentAreaStartPosition = getNextFigureAreaPosition();
+    let parentAreaSize = getNextFigureAreaSize();
+    let figureWidthPx = getFigureWidth(figure) * (CELL_SIZE + GAP_SIZE);
+    let figureHeightPx = getFigureHeight(figure) * (CELL_SIZE + GAP_SIZE);
+    return {
+        x: parentAreaStartPosition.x + parentAreaSize.width / 2 - figureWidthPx / 2,
+        y: parentAreaStartPosition.y + parentAreaSize.height / 2 - figureHeightPx / 2,
+    };
 }
 
 function drawText(
